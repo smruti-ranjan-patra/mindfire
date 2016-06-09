6857d882b65555a515439d4eb33cb8da0c0547a6
Smruti Ranjan <smruti94srp@gmail.com>
2016-06-09 16:56:45 +0530

    form with php v2

--- Profile_App/submit.php
+++ Profile_App/submit.php
@@ -1,49 +1,88 @@
-<?php
-// echo "Successfully Submitted";
-// echo '<pre>';
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Submit Page</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/form.css">
+</head>
+<body>
 
-// print_r($_POST);
-echo "<h1><u>Details  :-</u></h1><br>";
+	<div class="container">
+		<h1><u>Details  :-</u></h1>
 
-echo 'First Name : ' . $_POST['first_name']."<br>";
-echo 'Middle Name : ' . $_POST['middle_name']."<br>";
-echo 'Last Name : ' . $_POST['last_name']."<br>";
-echo 'prefix : ' . $_POST['prefix']."<br>";
-echo 'Gender : ' . $_POST['gender']."<br>";
-echo 'Date of Birth : ' . $_POST['dob']."<br>";
-echo 'Marital Status : ' . $_POST['marital']."<br>";
-echo 'Employment Status: ' . $_POST['employment']."<br>";
-echo 'Employer : ' . $_POST['employer']."<br>";
+        <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-10 col-lg-10 col-md-offset-1 col-lg-offset-1">
+          	<div class="well">
+                    <div class="row form-group">
+	                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
+	                    	<?php
 
-echo "<h3><u>Residence Address  :-</u></h3><br>";
+	                    	echo "<h3><u>General Details :-</u></h3><br>";
+							echo 'First Name : ' . $_POST['first_name']."<br>";
+							echo 'Middle Name : ' . $_POST['middle_name']."<br>";
+							echo 'Last Name : ' . $_POST['last_name']."<br>";
+							echo 'prefix : ' . $_POST['prefix']."<br>";
+							echo 'Gender : ' . $_POST['gender']."<br>";
+							echo 'Date of Birth : ' . $_POST['dob']."<br>";
+							echo 'Marital Status : ' . $_POST['marital']."<br>";
+							echo 'Employment Status: ' . $_POST['employment']."<br>";
+							echo 'Employer : ' . $_POST['employer']."<br>";
 
-echo 'Street : ' . $_POST['r_street']."<br>";
-echo 'City : ' . $_POST['r_city']."<br>";
-echo 'State : ' . $_POST['r_state']."<br>";
-echo 'Zip : ' . $_POST['r_zip']."<br>";
-echo 'Phone : ' . $_POST['r_phone']."<br>";
-echo 'Fax : ' . $_POST['r_fax']."<br>";
+	                    	?>
+	                    </div>
 
-echo "<h3><u>Office  :-</u></h3><br>";
+	                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
+	                    	<?php
 
-echo 'Street : ' . $_POST['o_street']."<br>";
-echo 'City : ' . $_POST['o_city']."<br>";
-echo 'State : ' . $_POST['o_state']."<br>";
-echo 'Zip : ' . $_POST['o_zip']."<br>";
-echo 'Phone : ' . $_POST['o_phone']."<br>";
-echo 'Fax : ' . $_POST['o_fax']."<br>";
+	                    	echo "<h3><u>Residence Address  :-</u></h3><br>";
+							echo 'Street : ' . $_POST['r_street']."<br>";
+							echo 'City : ' . $_POST['r_city']."<br>";
+							echo 'State : ' . $_POST['r_state']."<br>";
+							echo 'Zip : ' . $_POST['r_zip']."<br>";
+							echo 'Phone : ' . $_POST['r_phone']."<br>";
+							echo 'Fax : ' . $_POST['r_fax']."<br>";
 
-echo "<h3><u>Personal Info  :-</u></h3><br>";
+	                    	?>
+	                    </div>
 
-echo 'Picture : ' . $_POST['pic']."<br>";
-echo 'Notes : ' . $_POST['notes']."<br>";
+	                    <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3 col-sm-offset-1 col-md-offset-1 col-lg-offset-1">
+	                    	<?php
 
- if (isset($_POST['comm']))
- {
- 	echo 'Communication : ' . implode(', ', $_POST['comm'])."<br>";
- }
- else
- 	echo 'Please provide communication medium';
+	                    	echo "<h3><u>Office Address :-</u></h3><br>";
+							echo 'Street : ' . $_POST['o_street']."<br>";
+							echo 'City : ' . $_POST['o_city']."<br>";
+							echo 'State : ' . $_POST['o_state']."<br>";
+							echo 'Zip : ' . $_POST['o_zip']."<br>";
+							echo 'Phone : ' . $_POST['o_phone']."<br>";
+							echo 'Fax : ' . $_POST['o_fax']."<br>";
 
+	                    	?>
+	                    </div>
+	                </div>
 
-?>
+	                <div class="row form-group">
+	                    <div class="col-xs-12 col-sm-12 col-md-11 col-lg-11 col-md-offset-1 col-lg-offset-1">
+
+	                    	<?php
+
+	                    		echo "<h3><u>Personal Info  :-</u></h3><br>";
+								echo 'Picture : ' . $_POST['pic']."<br>";
+								echo 'Notes : ' . $_POST['notes']."<br>";
+
+								 if (isset($_POST['comm']))
+								 {
+								 	echo 'Communication : ' . implode(', ', $_POST['comm'])."<br>";
+								 }
+								 else
+								 	echo 'Please provide communication medium';
+
+	                    	?>
+
+	                    </div>
+                    </div>
+            </div>        	
+          </div>
+        </div>
+    </div>
+</body>
+</html>
